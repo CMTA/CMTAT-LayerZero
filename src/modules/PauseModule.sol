321cae5a451ed0538f8c10ac869053d6e6440b9a
Ryan Sauge <71391932+rya-sge@users.noreply.github.com>
2026-02-02 13:55:46 +0100

    Create PauseModule, add comments

---
+++ src/modules/PauseModule.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+/* ==== OpenZeppelin === */
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+
+abstract contract PauseModule is Pausable {
+    modifier onlyPauseManager() {
+        _authorizePause();
+        _;
+    }
+    /*//////////////////////////////////////////////////////////////
+                                PUBLIC
+    //////////////////////////////////////////////////////////////*/
+
+    function pause() public onlyPauseManager {
+        _pause();
+    }
+
+    function unpause() public onlyPauseManager {
+        _unpause();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         INTERNAL
+    //////////////////////////////////////////////////////////////*/
+
+    /* ==== AUTHORIZATION HOOKS === */
+    function _authorizePause() internal view virtual;
+}
